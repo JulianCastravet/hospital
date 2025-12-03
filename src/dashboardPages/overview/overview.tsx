05409948fc5433c8733696295ab3ba90af33a784
Anonymous2910 <castravet.iulian@gmail.com>
2025-12-03 15:24:55 +0200

    ver12

--- src/dashboardPages/overview/overview.tsx
+++ src/dashboardPages/overview/overview.tsx
@@ -13,6 +13,8 @@ export const Overview = () => {
   const { users, getUsers } = useUserStore();
   const { reports, getAllReports } = useReportStore();
 
+  console.log(reports)
+
   useEffect(() => {
     getUsers(message);
     getAllReports({ page: 1, pageSize: 1000000000 }, message);
